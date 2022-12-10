de6d833e960733116b28c8385fdfdc755a64f4fb
minhtranit99 <minhtran.it99@gmail.com>
2022-12-10 21:03:25 +0700

    add

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -80,7 +80,7 @@ export default {
                 {
                     hid: 'og-description',
                     property: 'og:description',
-                    content: '...',
+                    content: '&#12644; &#x3164;',
                 },
 
                 {
